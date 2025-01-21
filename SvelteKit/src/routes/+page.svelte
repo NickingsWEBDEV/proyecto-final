b84d06b657b6dbede8dc0473ee28248858083216
D`Anthony Brown <danthonybrown15@gmail.com>
2025-01-20 21:31:37 -0600

    realice un testeo justo despues de la clase

--- SvelteKit/src/routes/+page.svelte
+++ SvelteKit/src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1 class="bg-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<a href="./+page.svelte">devuelta al OG</a>
--- SvelteKit/src/routes/iniciar-sesion/+page.svelte
+++ SvelteKit/src/routes/iniciar-sesion/+page.svelte
@@ -1,1 +1,2 @@
 <h1 class="bg-red-500">hola mundo</h1>
+<a href="./+page.svelte">devuelta al OG</a>
