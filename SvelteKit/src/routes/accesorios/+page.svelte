9373800e45914429dc47e9acff199094074922b2
D`Anthony Brown <danthonybrown15@gmail.com>
2025-01-30 14:29:24 -0600

    empece a agregar el contenido de algunas pestañas

--- SvelteKit/src/routes/accesorios/+page.svelte
+++ SvelteKit/src/routes/accesorios/+page.svelte
@@ -1,2 +1,16 @@
-<h1 class="bg-orange-500">naranja</h1>
-<a href="/">devuelta al OG</a>
+<main class="flex min-h-screen items-center justify-center bg-neutral-300">
+	<div class="flex-cols flex gap-64">
+		<!-- caja de objetos -->
+		<div class="bg-amber-200 p-3 shadow-sm shadow-black">
+			<button class="flex items-end rounded bg-amber-500 p-2 text-white">Pasta Termica</button>
+		</div>
+
+		<div class="bg-amber-200 p-3 shadow-sm shadow-black">
+			<button class="flex items-end rounded bg-amber-500 p-2 text-white">Pasta Termica</button>
+		</div>
+
+		<div class="bg-amber-200 p-3 shadow-sm shadow-black">
+			<button class="flex items-end rounded bg-amber-500 p-2 text-white">Pasta Termica</button>
+		</div>
+	</div>
+</main>
