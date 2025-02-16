8a2e4ab35a86c8c26b90f4fc64c178d5aff20b10
D`Anthony Brown <danthonybrown15@gmail.com>
2025-02-16 03:32:42 -0600

    agregue la pagina  para administrar los productos, finalizando el esqueleto del proyecto. Solo queda conectar a la base de datos

---
+++ SvelteKit/src/lib/components/productos.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		class?: string;
+		children: Snippet;
+	};
+
+	const { class: className = '', children }: Props = $props();
+</script>
